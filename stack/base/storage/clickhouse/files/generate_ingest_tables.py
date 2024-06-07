3ef1569ed7f0f138f831b0b9ddb8c4f70d43b9b8
Marc Sune <marcdevel@gmail.com>
2024-06-07 23:38:39 +0200

    storage/clickhouse: adapt to YAML conf file/lib
    This commit adapts the job creating the ingestion tasks in clickhouse
    to use the python library for datahangar's YAML conf file.
    
    Yeaaah!

--- stack/base/storage/clickhouse/files/generate_ingest_tables.py
+++ stack/base/storage/clickhouse/files/generate_ingest_tables.py
@@ -5,36 +5,36 @@ from jinja2 import Environment, FileSystemLoader
 import os, sys
 import subprocess
 
-sys.path.append("/etc/datahangar/")
-from dh_conf_parser_lib import *
-
 DH_CONF_FOLDER="/etc/datahangar/"
 DRY_RUN=False
 
-def gen_ingest_tables(name, pipeline, cols, template):
+sys.path.append(DH_CONF_FOLDER)
+sys.path.append(DH_CONF_FOLDER+"/lib/")
+from dh_conf import datahangar_load_conf, datahangar_pipeline_db_get_cols
+
+def gen_ingest_tables(conf, name, pipeline, template):
     #Generate and add KAFKA secrets
     data = {
-        'kafkaDbIngestionTopic': pipeline["kafka.dbIngestionTopic"],
-        'dbTableName': pipeline["dbTableName"],
-        'clickhouse_cols': cols
+        'kafkaDbIngestionTopic': pipeline["kafka"]["topic-db-ingestion"],
+        'dbTableName': pipeline["db"]["table-name"],
+        'clickhouse_cols': datahangar_pipeline_db_get_cols(conf, pipeline, "clickhouse")
     }
     return template.render(data)
 
 if __name__ == "__main__":
-    pipelines=get_pipelines()
+    conf = datahangar_load_conf(DH_CONF_FOLDER)
 
     file_content=""
-    template = Environment(loader=FileSystemLoader('/tmp/conf')).get_template('create_tables_script.sql.j2')
-    for name, pipeline in pipelines.items():
+    template = Environment(loader=FileSystemLoader('/tmp/gen/')).get_template('create_tables_script.sql.j2')
+
+    for name, pipeline in conf["pipelines"].items():
         #Ignore disabled pipelines
-        if not pipeline["enabled"]:
-            print(f"WARNING: pipeline '{pipeline}' is disabled")
+        if "disabled" in pipeline and pipeline["disabled"]:
+            print(f"WARNING: pipeline '{name}' is disabled")
             continue
 
-        cols = get_db_cols("clickhouse", pipeline)
-
         print(f"Generating '{name}' ingestion tables...")
-        file_content=file_content+gen_ingest_tables(name, pipeline, cols, template)
+        file_content=file_content+gen_ingest_tables(conf, name, pipeline, template)
 
     #Create tmp file
     with open("/tmp/create_tables_script.sql", 'w') as f:
