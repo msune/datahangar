f97afcf4b96f646189e6ee09f7b3be2c0134d7d2
Marc Sune <marcdevel@gmail.com>
2024-06-08 00:40:11 +0200

    conf|ui/frontend: dh-users.yaml and initContainer
    This patch:
    
    * Moves users into a file conf/dh-users.yaml
    * Changes frontend config generation to use initContainer
    
    Yup, yup! AHYEAH

---
+++ stack/base/ui/frontend/files/gen_htpasswd.py
@@ -0,0 +1,16 @@
+import yaml
+from passlib.apache import HtpasswdFile
+import os
+
+with open('/tmp/dh-users.yaml', 'r') as file:
+    config = yaml.safe_load(file)
+
+htpasswd = HtpasswdFile()
+
+for user in config['users']:
+    username = user['name']
+    password = user['password']
+    htpasswd.set_password(username, password)
+
+htpasswd.save('/etc/nginx/htpasswd/htpasswd')
+print("htpasswd file created successfully.")
