f57b688092e4dd064a35e3c88d394290bdb05876
Marc Sune <marcdevel@gmail.com>
2024-06-07 23:01:31 +0200

    ui/turnilo: adapt to YAML conf file/lib
    This commit adapts turnilo config generation to the python library
    for datahangar's YAML conf file and also to use initContainers.
    
    Aha!

--- stack/base/ui/turnilo/files/gen_turnilo_conf.py
+++ stack/base/ui/turnilo/files/gen_turnilo_conf.py
@@ -5,24 +5,29 @@ from jinja2 import Environment, FileSystemLoader
 import sys, os
 import requests
 
-sys.path.append("/etc/datahangar/")
-from dh_conf_parser_lib import *
+DH_CONF_FOLDER="/etc/datahangar/"
 
 if __name__ == "__main__":
-    pipelines=get_pipelines()
+    sys.path.append(DH_CONF_FOLDER)
+    sys.path.append(DH_CONF_FOLDER+"/lib/")
+    from dh_conf import datahangar_load_conf, datahangar_pipeline_db_get_cols
 
-    template = Environment(loader=FileSystemLoader('/tmp/conf')).get_template('turnilo_conf.yaml.j2')
-
+    conf = datahangar_load_conf("/etc/datahangar/")
     data_cubes = {}
-    for name, pipeline in pipelines.items():
-        if not pipeline["enabled"]:
-            print(f"WARNING: pipeline '{pipeline}' is disabled")
+    for name, pipeline in conf["pipelines"].items():
+        if "disabled" in pipeline and pipeline["disabled"]:
+            print(f"WARNING: pipeline '{name}' is disabled")
             continue
-        cols = get_db_cols("druid", pipeline)
+        cols = datahangar_pipeline_db_get_cols(conf, pipeline, "druid")
         data_cubes[name] = {}
-        data_cubes[name]["db_table_name"] = pipeline["dbTableName"]
+        data_cubes[name]["title"] = pipeline["title"]
         data_cubes[name]["description"] = pipeline["description"]
+        data_cubes[name]["db_table_name"] = pipeline["db"]["table-name"]
         data_cubes[name]["cols"] = cols
 
-    with open("/turnilo_config.yaml", 'w') as f:
-        f.write(template.render({"data_cubes" : data_cubes}))
+    template = Environment(loader=FileSystemLoader('/tmp/')).get_template('turnilo_conf.yaml.j2')
+    conf_file = template.render({"data_cubes" : data_cubes})
+    with open("/etc/turnilo/config.yaml", 'w') as f:
+        f.write(conf_file)
+    print(f"Generated configuration file:\n")
+    print(conf_file)
